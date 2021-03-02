421eb921ddd4c6bbf547668e06028b2c99d9afff
Izabela Tomaszewska <itldzpl@gmail.com>
2021-03-02 10:17:12 +0100

    Second Commit with full app

---
+++ FizzBuzzVerification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzVerification
+    {
+        
+        public string GetResult { get { return _results; }}
+        private string _results;
+        public FizzBuzzVerification()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int input))
+            {
+                throw new Exception($"{Environment.NewLine} Please provide the number as an input...");
+            }
+            _results= input % 2 == 0 ? input % 5 == 0 ? "FizzBuzz" : "Fizz" : input % 5 == 0 ? "Buzz" : input.ToString();
+        }
+
+    } 
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainMethod();
+        }
+
+        private static void MainMethod()
+        {
+            //Operate over user input
+            try
+            {
+                // Ask nicely
+                Console.WriteLine($"{Environment.NewLine} Hello! { Environment.NewLine} Please provide one number...");
+                //Collect input and veriify the number
+                Console.WriteLine(new FizzBuzzVerification().GetResult);
+                //Ask what to do next?
+                UserActionsManagment();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                MainMethod();
+            }
+        }
+
+        private static void UserActionsManagment()
+        {
+            //What do you want to do next?
+            Console.WriteLine("What do you want to do next ? For exit press (e) to continue press (c).");
+            //Perfomr an action depend on user input
+            var keyInput = Console.ReadKey().Key.ToString().ToLower();
+            switch (keyInput)
+            {
+                case "e":
+                    Environment.Exit(0);
+                    break;
+                case "c":
+                    MainMethod();
+                    break;
+                default:
+                    Console.WriteLine($"{Environment.NewLine} No appropriate action chosen!");
+                    UserActionsManagment();
+                    break;
+            }
+        }
+    }
+}
